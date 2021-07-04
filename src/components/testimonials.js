f685717295cf224449fb5cc46c0d253046a76cb7
Murodali <smurodali@gmail.com>
2021-07-04 16:32:50 +0600

    finaltest

--- src/components/testimonials.js
+++ src/components/testimonials.js
@@ -33,7 +33,7 @@ export default class testimonials extends Component {
                                     <div className="info">
                                         <h3>Umed Babakhanov</h3>
                                         <p><b>CEO of ASIA PLUS, Tajkistan</b></p>
-                                        <p><b>Murodali is an exceptional induvial. He worked at Asia Plus as intern and later implement seeral web developmet projects. He is highly creative, responsive and reliable. I enjoyed working with and looking forward to hire him for further projects </b></p>
+                                        <h1>"</h1><p><b>Murodali is an exceptional induvial. He worked at Asia Plus as intern and later implement seeral web developmet projects. He is highly creative, responsive and reliable. I enjoyed working with and looking forward to hire him for further projects </b></p><h1>"</h1>
                                     </div>
 
                                 </Carousel.Item>
@@ -46,7 +46,7 @@ export default class testimonials extends Component {
                                     <div className="info">
                                         <h3>Erik Kraus</h3>
                                         <p><b>Dean of Student Success at Sampoerna University </b></p>
-                                        <p> <b> Murodali is an outstanding student who has a strong entrepreneurial orientation.  I worked with him for four years at the University of Central Asia and found him to be intelligent, dependable, and highly reliable.  I would certainly hire him to work on one of my projects.</b></p>
+                                        <h1>"</h1> <p> <b> Murodali is an outstanding student who has a strong entrepreneurial orientation.  I worked with him for four years at the University of Central Asia and found him to be intelligent, dependable, and highly reliable.  I would certainly hire him to work on one of my projects.</b></p><h1>"</h1>
                                     </div>
 
                                 </Carousel.Item>
